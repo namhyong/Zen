03c6f20278937bde21c03da40aa2056bcef3ae47
namhyong <namhyong2@gmail.com>
2025-05-19 22:59:40 +0900

    feat: modify

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Programs from "./pages/Programs";
+// import About from "./pages/About";
+// import Programs from "./pages/Programs";
 import Contact from "./pages/Contact";
 import Subscribe from "./pages/Subscribe";
-import HeroSection from "./components/HeroSection";
-import ProgramSection from "./components/ProgramSection";
+// import HeroSection from "./components/HeroSection";
+// import ProgramSection from "./components/ProgramSection";
 import Checkout from "./pages/CheckOut";
 import ProgramDetail from "./pages/ProgramDetail";
 export default function App() {
